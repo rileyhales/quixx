4be42266e77f51f4303321a6c04a6b5e36a5ef8a
Riley Hales <rileyhales1@gmail.com>
2021-03-22 22:52:32 -0600

    count scores and total in bottom box

--- src/Quixx.js
+++ src/Quixx.js
@@ -1,15 +1,85 @@
 import React from "react";
 
 import NavBar from "./NavBar";
-import ScoreCard from "./ScoreCard";
+import ScoreTotals from "./ScoreTotals";
+import ScoreGroup from "./ScoreGroup";
 
-const Quixx = function () {
-    return (
-        <div>
-            <NavBar/>
-            <ScoreCard/>
-        </div>
-    )
+const numberOfButtons = 12
+
+class Quixx extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            checkedButtons: {
+                red: Array(numberOfButtons).fill(false),
+                yellow: Array(numberOfButtons).fill(false),
+                green: Array(numberOfButtons).fill(false),
+                blue: Array(numberOfButtons).fill(false),
+            },
+            checks: {
+                red: 0,
+                yellow: 0,
+                green: 0,
+                blue: 0
+            },
+            scores: {
+                red: 0,
+                yellow: 0,
+                green: 0,
+                blue: 0,
+                total: 0,
+            },
+            ascend: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            descend: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+        }
+        this.handleScoreButtonClick = this.handleScoreButtonClick.bind(this)
+    }
+
+    handleScoreButtonClick = function (event) {
+        // disable clicked button
+        let buttonid = event.target.id.split("-")
+        event.target.disabled = true
+        event.target.className += (" score-btn-scored")
+
+        // disable the buttons before the clicked one
+        if (buttonid[0] === "red" || buttonid[0] === "yellow") {
+            for (let i = 0; i <= this.state.ascend.indexOf(Number(buttonid[1])); i++) {
+                document.getElementById(`${buttonid[0]}-${this.state.ascend[i]}`).disabled = true
+            }
+        } else if (buttonid[0] === "green" || buttonid[0] === "blue") {
+            for (let i = 0; i <= this.state.descend.indexOf(Number(buttonid[1])); i++) {
+                document.getElementById(`${buttonid[0]}-${this.state.descend[i]}`).disabled = true
+            }
+        }
+
+        // set the state with the changes made
+        this.setState(previousState => {
+            let checks = previousState.checks
+            checks[buttonid[0]] += 1
+            let scores = previousState.scores
+            scores[buttonid[0]] += checks[buttonid[0]]
+            scores.total = scores.red + scores.yellow + scores.green + scores.blue
+            return {
+                "checks": checks,
+                "score": scores
+            }
+        })
+    }
+
+    render() {
+
+        return (<div>
+                <NavBar/>
+                <div key={"scorecard"} className="score-card">
+                    <ScoreGroup order={this.state.ascend} color="red" handleScoreButtonClick={this.handleScoreButtonClick}/>
+                    <ScoreGroup order={this.state.ascend} color="yellow" handleScoreButtonClick={this.handleScoreButtonClick}/>
+                    <ScoreGroup order={this.state.descend} color="green" handleScoreButtonClick={this.handleScoreButtonClick}/>
+                    <ScoreGroup order={this.state.descend} color="blue" handleScoreButtonClick={this.handleScoreButtonClick}/>
+                </div>
+                <ScoreTotals scores={this.state.scores}/>
+            </div>
+        )
+    }
 }
 
 export default Quixx
--- src/ScoreTotals.js
+++ src/ScoreTotals.js
@@ -2,11 +2,16 @@ import React from "react"
 
 const ScoreTotals = function (props) {
     return (
-        <div className="score-totals">
-            <button>{props.scores.red}</button>
-            <button>{props.scores.yellow}</button>
-            <button>{props.scores.green}</button>
-            <button>{props.scores.blue}</button>
+        <div className="score-totals nav-fill">
+            <button className="score-btn bg-quixx-red">{props.scores.red}</button>
+            <div>+</div>
+            <button className="score-btn bg-quixx-yellow">{props.scores.yellow}</button>
+            <div>+</div>
+            <button className="score-btn bg-quixx-green">{props.scores.green}</button>
+            <div>+</div>
+            <button className="score-btn bg-quixx-blue">{props.scores.blue}</button>
+            <div>=</div>
+            <button className="score-btn">{props.scores.total}</button>
         </div>
     )
 }
