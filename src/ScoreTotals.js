b68c1d18e917320ee06ae9f0919d4b96de4eb83e
Riley Hales <rileyhales1@gmail.com>
2021-04-10 16:20:11 -0600

    add skip buttons

--- src/ScoreTotals.js
+++ src/ScoreTotals.js
@@ -3,15 +3,22 @@ import React from "react"
 const ScoreTotals = function (props) {
     return (
         <div className="score-totals nav-fill">
-            <button className="score-btn bg-quixx-red">{props.scores.red}</button>
+            <button className="score-total-btn bg-quixx-red">{props.scores.red}</button>
             <div>+</div>
-            <button className="score-btn bg-quixx-yellow">{props.scores.yellow}</button>
+            <button className="score-total-btn bg-quixx-yellow">{props.scores.yellow}</button>
             <div>+</div>
-            <button className="score-btn bg-quixx-green">{props.scores.green}</button>
+            <button className="score-total-btn bg-quixx-green">{props.scores.green}</button>
             <div>+</div>
-            <button className="score-btn bg-quixx-blue">{props.scores.blue}</button>
+            <button className="score-total-btn bg-quixx-blue">{props.scores.blue}</button>
             <div>=</div>
-            <button className="score-btn">{props.scores.total}</button>
+            <button className="score-total-btn">{props.scores.total}</button>
+            <div>
+                Skips
+            </div>
+            <input type={"checkbox"}/>
+            <input type={"checkbox"}/>
+            <input type={"checkbox"}/>
+            <input type={"checkbox"}/>
         </div>
     )
 }
