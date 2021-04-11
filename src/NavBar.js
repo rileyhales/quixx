2597201058355baad25daa24ff9b9dae51d34669
Riley Hales <rileyhales1@gmail.com>
2021-04-10 18:27:05 -0600

    update naming conventions

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,11 @@
 import React from "react"
-import TimeTravelButtons from "./TimeTravelButtons";
+import NavButtons from "./NavButtons";
 
 const NavBar = function (props) {
     return (
         <nav className="nav nav-fill quixx-nav bg-quixx-orange">
             <h1 className="nav-title">Quixx</h1>
-            <TimeTravelButtons
+            <NavButtons
                 canUndo={props.canUndo}
                 canRedo={props.canRedo}
                 handleUndoButtonClick={props.handleUndoButtonClick}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import Quixx from "./Quixx"
 
 import "./index.css"
-import "./colors.css"
+import "./quixx-colors.css"
 import "./quixx-header.css"
 import "./quixx-scorecard.css"
 import "./quixx-footer.css"
