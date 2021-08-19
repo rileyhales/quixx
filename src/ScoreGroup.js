dbab972bd95918d2b57a5d2f0d3b5b7fa3ca8cb5
Riley Hales <rileyhales1@gmail.com>
2021-08-18 22:38:39 -0600

    wrap score buttons to style separately

--- src/ScoreGroup.js
+++ src/ScoreGroup.js
@@ -4,20 +4,22 @@ import "./ScoreGroup.css"
 
 const ScoreGroup = (props) => {
     return (
-        <div className={`score-group bg-quixx-${props.color}`}>
+        <div className={`score-group`}>
             {
                 props.state.order.map((number, index) => {
                     let classes = ["score-button", "button-sizes"]
                     if (props.state.scored[index]) classes.push("scored")
                     if (number === "lock" && !props.state.scored[10]) classes.push("locked")
                     return (
-                        <button key={index}
-                                className={classes.join(" ")}
-                                disabled={!props.state.canClick[index]}
-                                onClick={() => props.click(props.color, number)}
-                                aria-label={"scoring button"}>
-                            {number === "lock" ? <img className={"lock-icon"} src={process.env.PUBLIC_URL + "/lock-fill.svg"} alt={"lock icon"}/> : number}
-                        </button>
+                        <div className={`bg-quixx-${props.color} score-button-wrapper`}>
+                            <button key={index}
+                                    className={classes.join(" ")}
+                                    disabled={!props.state.canClick[index]}
+                                    onClick={() => props.click(props.color, number)}
+                                    aria-label={"scoring button"}>
+                                {number === "lock" ? <img className={"lock-icon"} src={process.env.PUBLIC_URL + "/lock-fill.svg"} alt={"lock icon"}/> : number}
+                            </button>
+                        </div>
                     )
                 })
             }
